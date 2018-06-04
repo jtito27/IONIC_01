cad7c374c6cbc1abf0f0e044d6ef27384f379124
João Victor Veronesi <36904929+jtito27@users.noreply.github.com>
2018-06-04 12:31:43 -0300

    primeiro commit

---
+++ src/pages/lista-paises/lista-paises.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the ListaPaisesPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-lista-paises',
+  templateUrl: 'lista-paises.html',
+})
+export class ListaPaisesPage {
+
+  
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+
+}
